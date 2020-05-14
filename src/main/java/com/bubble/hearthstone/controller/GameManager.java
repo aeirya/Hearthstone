ca10e7117236acd7a02d5ca76a21e3d794e24b30
aeirya <aeirya@gmail.com>
2020-05-15 01:53:35 +0430

    user manager logs into guest automatically (rebase this)

--- src/main/java/com/bubble/hearthstone/controller/GameManager.java
+++ src/main/java/com/bubble/hearthstone/controller/GameManager.java
@@ -6,7 +6,9 @@ import com.bubble.hearthstone.net.event.EventHandler;
 import com.bubble.hearthstone.net.event.GameEventHandler;
 import com.bubble.hearthstone.net.event.IGameEvent;
 import com.bubble.hearthstone.net.event.events.BroadcastMessageEvent;
+import com.bubble.hearthstone.net.event.events.DeleteUserEvent;
 import com.bubble.hearthstone.net.event.events.LoginEvent;
+import com.bubble.hearthstone.net.event.events.SignupEvent;
 import com.bubble.hearthstone.net.user.UserManager;
 import com.bubble.hearthstone.util.services.ServiceLocator;
 
@@ -18,8 +20,7 @@ public class GameManager {
 
     public GameManager() {
         userManager = new UserManager();
-        userManager.login("guest", "");
-        eventHandler = new GameEventHandler(this, userManager.getUser()).start();
+        eventHandler = new GameEventHandler(this, userManager).start();
         network = new DummyNetworkEventQueue();
         test();
     }
@@ -28,6 +29,14 @@ public class GameManager {
         return userManager.login(username, password);
     }
 
+    public boolean signup(String username, String password) {
+        return userManager.signup(username, password);
+    }
+
+    public boolean deleteUser(String username, String password) {
+        return userManager.deleteUser(username, password);
+    }
+    
     public void printBroadcastMessage(String message) {
         ServiceLocator.getLogger().log("broadcast message: " + message);
     }
@@ -44,12 +53,43 @@ public class GameManager {
         eventHandler.receive(event);
     }
 
-    private void test() {
+    private void  test() {
+        registerTest();
+        loginTest();
+        newUserTest();
+        deleteUserTest();
+    }
+
+    private void registerTest() {
+        network.push(
+            new SignupEvent("aeirya", "123")
+        );
+        network.push(
+            new SignupEvent("aeirya", "1234")
+        );
+    }
+
+    private void loginTest() {
         network.push(
             new LoginEvent("aeirya", "123")
         );
         network.push(
             new LoginEvent("aeirya", "1234")
+        );
+    }
+
+    private void newUserTest() {
+        network.push(
+            new SignupEvent("newUser", "newPassword")
+        );
+    }
+
+    private void deleteUserTest() {
+        network.push(
+            new DeleteUserEvent("aeirya", "1234")
+        );
+        network.push(
+            new DeleteUserEvent("aeirya", "123")
         );
     }
 }
