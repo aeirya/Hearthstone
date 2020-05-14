e9966c2032631042fdc040c9b0c319f26cc89287
aeirya <aeirya@gmail.com>
2020-05-14 12:46:38 +0430

    ui interfaces

---
+++ src/main/java/com/bubble/hearthstone/ui/IGameGraphics.java
@@ -0,0 +1,11 @@
+package com.bubble.hearthstone.ui;
+
+import com.bubble.hearthstone.interfaces.Updatable;
+
+public interface IGameGraphics extends Updatable {
+    /**
+     * maybe:
+     *  use LWJGL 3
+     */    
+    void message(String message);
+}
---
+++ src/main/java/com/bubble/hearthstone/ui/SwingGraphics.java
@@ -0,0 +1,13 @@
+package com.bubble.hearthstone.ui;
+
+public class SwingGraphics implements IGameGraphics {
+
+    //wokring on it on gui branch
+    public void update() {
+        //
+    }
+
+    public void message(String message) {
+        //
+    }
+}
