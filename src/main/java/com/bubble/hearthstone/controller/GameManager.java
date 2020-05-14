2dca8f5473b3fdd6d879933abe61424ea3bb7520
aeirya <aeirya@gmail.com>
2020-05-14 14:37:56 +0430

    (squash this) added game event handler

--- src/main/java/com/bubble/hearthstone/controller/GameManager.java
+++ src/main/java/com/bubble/hearthstone/controller/GameManager.java
@@ -3,6 +3,7 @@ package com.bubble.hearthstone.controller;
 import com.bubble.hearthstone.net.event.DummyNetworkEventQueue;
 import com.bubble.hearthstone.net.event.INetworkEventQueue;
 import com.bubble.hearthstone.net.event.EventHandler;
+import com.bubble.hearthstone.net.event.GameEventHandler;
 import com.bubble.hearthstone.net.event.IGameEvent;
 import com.bubble.hearthstone.net.event.events.BroadcastMessageEvent;
 import com.bubble.hearthstone.net.user.UserManager;
@@ -12,11 +13,11 @@ public class GameManager {
     
     private final UserManager userManager;
     private final EventHandler eventHandler;
-    private final INetworkEventQueue network;
+    protected final INetworkEventQueue network;
 
     public GameManager() {
         userManager = new UserManager();
-        eventHandler = new EventHandler(this);
+        eventHandler = new GameEventHandler(this);
         network = new DummyNetworkEventQueue();
     }
 
