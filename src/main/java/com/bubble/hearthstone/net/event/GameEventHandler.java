90e4087a0dc2cabd949fe56c431013d4da6a4654
aeirya <aeirya@gmail.com>
2020-05-14 21:57:14 +0430

    trying to implemet event loggers

--- src/main/java/com/bubble/hearthstone/net/event/GameEventHandler.java
+++ src/main/java/com/bubble/hearthstone/net/event/GameEventHandler.java
@@ -3,10 +3,11 @@ package com.bubble.hearthstone.net.event;
 import com.bubble.hearthstone.controller.GameManager;
 import com.bubble.hearthstone.net.user.User;
 import com.bubble.hearthstone.util.log.EventLogger;
+import com.bubble.hearthstone.util.log.IEventLogger;
 import com.bubble.hearthstone.util.services.ServiceLocator;
 
 /** also logs event */
-public class GameEventHandler extends EventHandler {
+public class GameEventHandler extends EventHandler implements IEventLogger {
 
     private final EventLogger logger;
 
@@ -18,6 +19,18 @@ public class GameEventHandler extends EventHandler {
     @Override
     protected void process(IGameEvent event) {
         event.process(manager);
+        log(event);
+    }
+    
+    public void log(IGameEvent event) {
         logger.log(event);
+    }
+
+    public void error(String error) {
+        logger.error(error);
+    }
+
+    public void success(String message) {
+        logger.success(message);
     }
 }
--- src/main/java/com/bubble/hearthstone/util/log/EventLogger.java
+++ src/main/java/com/bubble/hearthstone/util/log/EventLogger.java
@@ -3,7 +3,7 @@ package com.bubble.hearthstone.util.log;
 import com.bubble.hearthstone.net.event.IGameEvent;
 import com.bubble.hearthstone.net.user.User;
 
-public class EventLogger {
+public class EventLogger implements IEventLogger {
 
     private final GameLogger logger;
     private final MyFileWriter writer;
@@ -21,6 +21,14 @@ public class EventLogger {
                 new EventLog(event).toString()
             );
         }
+    }
+
+    public void error(String error) {
+        logger.warning(error);
+    }
+
+    public void success(String message) {
+        logger.info(message);
     }
 
     //TODO: implement the user change scenario
