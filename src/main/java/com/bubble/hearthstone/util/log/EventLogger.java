75d1a12ba94b2bc3e3f27401300908b6839c9e8f
aeirya <aeirya@gmail.com>
2020-05-15 00:56:48 +0430

    event logger gets the log path from resources

--- src/main/java/com/bubble/hearthstone/util/log/EventLogger.java
+++ src/main/java/com/bubble/hearthstone/util/log/EventLogger.java
@@ -3,21 +3,25 @@ package com.bubble.hearthstone.util.log;
 import com.bubble.hearthstone.net.event.IGameEvent;
 import com.bubble.hearthstone.net.user.User;
 import com.bubble.hearthstone.net.user.UserManager;
+import com.bubble.hearthstone.util.services.ServiceLocator;
 
 public class EventLogger implements IEventLogger {
 
     private final GameLogger logger;
     private final MyFileWriter writer;
     private final UserManager userManager;
+    private final String logPath;
 
     public EventLogger(GameLogger logger, UserManager userManager) {
         this.logger = logger;
         this.userManager = userManager;
+        this.logPath = ServiceLocator.getResources().getLogPath();
         writer = new MyFileWriter();
     }
 
     private String getWritePath(User user) {
-        return "data/logs/user-" + user.getUsername() + ".log";
+        return 
+            logPath + "user-" + user.getUsername() + ".log";
     }
 
     public void log(IGameEvent event) {
@@ -30,11 +34,9 @@ public class EventLogger implements IEventLogger {
     }
     
     private void writeToLogFile(IGameEvent event, String path) {
-        writer
-                .setPath(path)
-                .write(
-                    new EventLog(event).toString()
-            );
+        writer.setPath(path).write(
+            new EventLog(event).toString()
+        );
     }
 
     // maybe i just should use the service locator logger instead of these
