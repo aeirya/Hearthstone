cba02157579d8707a9e35838bef8d1c0c9aae556
aeirya <aeirya@gmail.com>
2020-05-15 03:03:55 +0430

    remade json loader

--- src/main/java/com/bubble/hearthstone/util/resource/JsonLoader.java
+++ src/main/java/com/bubble/hearthstone/util/resource/JsonLoader.java
@@ -3,19 +3,13 @@ package com.bubble.hearthstone.util.resource;
 import com.bubble.hearthstone.util.serialize.GsonSerializer;
 import com.bubble.hearthstone.util.serialize.ISerializer;
 
-public class JsonLoader <T> extends ResourceLoader <T> {
+public class JsonLoader {
 
-    public T loadFile(String path, Class<T> clazz)
+    public <T> T load(String path, Class<T> clazz)
     {
         final FileLoader fileLoader = new FileLoader();
         final String file = fileLoader.loadFile(path);
         final ISerializer ser = new GsonSerializer();
         return ser.deserialize(file, clazz);
-    }
-    
-    /** don't use this, use loadfile(String string, Class<T> clazz) instead
-     * @return null **/
-    public T loadFile(String path) {
-        return null;
     }
 }
