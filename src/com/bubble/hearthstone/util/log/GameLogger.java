aced23da45676e1712f701b214f540e2344af197
aeirya <aeirya@gmail.com>
2020-07-07 11:42:18 +0430

    added log package (except event logger)

---
+++ src/com/bubble/hearthstone/util/log/GameLogger.java
@@ -0,0 +1,42 @@
+package com.bubble.hearthstone.util.log;
+
+import java.util.Arrays;
+
+/** least features every game logger should have  */
+public class GameLogger extends AbstractLogger {
+
+    public GameLogger() {
+        this.logger = new JavaUtilLogger();
+    }
+
+    public GameLogger get(String name) {
+        set(name);
+        return this;
+    }
+
+    public void log(String message, String from) {
+        log(from + ": " + message);
+    }
+
+    public void error(String from) {
+        log("error!", from);
+    }
+
+    public void error(Object from, Throwable thrown) {
+        log(LogLevel.ERROR, from.getClass().getName(), thrown);
+    }
+
+    public void log(String... messages) {
+        log(concat(messages));
+    }
+
+    public void logEvent(String message) {
+        log(message);
+    }
+
+    private String concat(String... strings) {
+        final StringBuilder build = new StringBuilder();
+        Arrays.asList(strings).forEach(build::append);
+        return build.toString();
+    }
+}
